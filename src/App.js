ac05b8cb0f3b88e7da5e2e1ca887a4b5eb0ef20b
Stijn Van Hamme <stijnvanhamme@gmail.com>
2019-11-13 16:23:55 +0100

    routing start: add checkout summary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 
 function App() {
   return (
     <Layout>
       <BurgerBuilder />
+      <Checkout />
     </Layout>
   );
 }
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+import classes from './Checkout.module.css';
+
+class Checkout extends React.Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <CheckoutSummary ingredients={this.state.ingredients} />
+            </div>
+        );
+    }
+}
+
+export default Checkout;
